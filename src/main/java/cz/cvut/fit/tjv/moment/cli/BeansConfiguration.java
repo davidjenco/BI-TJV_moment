423d43c9f823a93d3d526abbf9e0a94dadb06be6
jencodav <jencodav@fit.cvut.cz>
2021-11-08 23:28:24 +0100

    Delete cli main

--- src/main/java/cz/cvut/fit/tjv/moment/cli/BeansConfiguration.java
+++
@@ -1,9 +0,0 @@
-package cz.cvut.fit.tjv.moment.cli;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-
-@Configuration
-@PropertySource("classpath:application.properties")
-public class BeansConfiguration {
-}
