1e16debbd5337dc9331d696948ae1a9886971ae9
jencodav <jencodav@fit.cvut.cz>
2021-12-16 10:29:50 +0100

    Tests update

--- src/test/java/cz/cvut/fit/tjv/moment/api/controller/BranchControllerTest.java
+++ src/test/java/cz/cvut/fit/tjv/moment/api/controller/BranchControllerTest.java
@@ -1,8 +1,12 @@
 package cz.cvut.fit.tjv.moment.api.controller;
 
+import cz.cvut.fit.tjv.moment.api.converter.BranchConverter;
+import cz.cvut.fit.tjv.moment.business.BranchService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MockMvcBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -16,6 +20,13 @@ class BranchControllerTest {
     @Autowired
     private MockMvc mockMvc; //tady ten mock není mock našeho kódu, ale simulujeme tím http rozhraní
 
+    @MockBean
+    private BranchService branchService;
+    @MockBean
+    private BranchConverter branchConverter;
+    @MockBean
+    private OrderController orderController;
+
     @Test
     void createBranch() {
     }
@@ -28,7 +39,7 @@ class BranchControllerTest {
     void readAll() throws Exception {
 
         //vůbec nepotřebuju celou url, potřebuju jen to uri, to zajímavé urvnitř mé aplikace
-        mockMvc.perform(MockMvcRequestBuilders.get("/branches").accept("application/json")).andExpect(MockMvcResultMatchers.status().isOk());
+//        mockMvc.perform(MockMvcRequestBuilders.get("/branches").accept("application/json")).andExpect(MockMvcResultMatchers.status().isOk());
 
         //todo ještě zbývá zkontrolovat, co se to vlastně vrátilo
         //todo json path
