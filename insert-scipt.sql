b3642ab2c6d9dd7b6db27e2bfda6a253d6291bd3
jencodav <jencodav@fit.cvut.cz>
2022-01-07 21:55:34 +0100

    Add insert script

---
+++ insert-scipt.sql
@@ -0,0 +1,11 @@
+insert into menu_item (id, name, price, alcoholic) values (1, 'Cappuco', 30, false);
+insert into menu_item (id, name, price, alcoholic) values (2, 'Sandwich', 50, false);
+insert into menu_item (id, name, price, alcoholic) values (3, 'Latte', 70, false);
+insert into menu_item (id, name, price, alcoholic) values (4, 'piccolo', 20, false);
+insert into menu_item (id, name, price, alcoholic) values (5, 'Bloody wine', 70, true);
+insert into menu_item (id, name, price, alcoholic) values (6, 'Water', 10, false);
+insert into menu_item (id, name, price, alcoholic) values (7, 'Baileys', 60, true);
+insert into menu_item (id, name, price, alcoholic) values (8, 'Juice', 30, false);
+insert into menu_item (id, name, price, alcoholic) values (9, 'Wafle', 50, false);
+
+insert into branch (id, lucky_num) values (1, 100);
